806586033db3dd06796ce7335f2f7471c3962b6a
Tyler Huang <thuang00@stuy.edu>
2018-11-07 23:02:32 -0500

    aa

--- WordSearch.java
+++ WordSearch.java
@@ -7,12 +7,9 @@ public class WordSearch{
      *@param col is the starting width of the WordSearch
      */
     public WordSearch(int rows,int cols){
+      if (rows < 0 || cols < 0) throw new ArrayIndexOutOfBoundsException();
       data = new char[rows][cols];
-      for (int i = 0; i < data.length; i++){
-        for (int j = 0; j < data[i].length; j++){
-          data[i][j] = '_';
-        }
-      }
+      clear();
     }
 
     /**Set all values in the WordSearch to underscores'_'*/
