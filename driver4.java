085135b6ae49c09239e7556de94d86ab16f37e74
Tyler Huang <thuang00@stuy.edu>
2018-11-13 22:08:52 -0500

    -a

--- driver4.java
+++ driver4.java
@@ -3,19 +3,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class driver4{
-  public static void main(String[] args) throws FileNotFoundException{
+  public static void main(String[] args){
     try{
       if (args.length > 0){
-          WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), "words.text");
-        }
+        
         if (args.length == 3){
+          System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\n" + "File: " + args[2]);
           WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]);
         }
+        if (args.length == 4){
+          WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]));
+        }
+        //if (args.length == 5){
+        //  WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(argss[3]), args[4]);
+        //}
+      }
       WordSearch p = new WordSearch(10,10, "words.txt");
       System.out.println(p.toString());
     }
-    catch(FileNotFoundException e){
-      System.out.println("Input a valid file name");
+    catch(IllegalArgumentException e){
+      System.out.println("Row or Column cannot be 0");
     }
   }
 }
