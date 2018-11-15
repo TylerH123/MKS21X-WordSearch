0f23a3c35e6997171332df9f5f8868d6c1e99bee
Tyler Huang <thuang00@stuy.edu>
2018-11-15 11:47:22 -0500

    a

--- driver4.java
+++ driver4.java
@@ -17,9 +17,9 @@ public class driver4{
           System.out.println(p.toString());
         }
         if (args.length == 5){
-          System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2] + "\nAnswer key: " + args[4]);
-          WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Boolean.parseBoolean(args[4]));
-          System.out.println(p.toString());
+            System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2] + "\nAnswer key: " + args[4]);
+            WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Boolean.parseBoolean(args[4]));
+            System.out.println(p.toString());
         }
       }
       else{
