710d27ba9a81d21c9af87d2292ab090d4d759d37
Tyler Huang <thuang00@stuy.edu>
2018-11-16 22:48:27 -0500

    a

--- driver4.java
+++ driver4.java
@@ -7,28 +7,33 @@ public class driver4{
     try{
       if (args.length > 2){
         if (args.length == 3){
-          System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\n" + "File: " + args[2]);
+          System.out.println("usage: java WordSearch [rows cols filename [randomSeed [answers]]]");
+          System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2]);
           WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]);
           System.out.println(p.toString());
         }
         if (args.length == 4){
-          System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2] + "\nNo answer key");
+          System.out.println("usage: java WordSearch [rows cols filename [randomSeed [answers]]]");
+          System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2] + "\nNo key");
           WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), false);
           System.out.println(p.toString());
         }
         if (args.length == 5){
-          if (args[4].equals("true") || args[4].equals("false")){
+          if (args[4].equals("key")){
+            System.out.println("usage: java WordSearch [rows cols filename [randomSeed [answers]]]");
             System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2] + "\nAnswer key: " + args[4]);
-            WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Boolean.parseBoolean(args[4]));
+            WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), true);
             System.out.println(p.toString());
           }
           else{
-            System.out.println("Please only use true or false for the fifth argument");
+            System.out.println("usage: java WordSearch [rows cols filename [randomSeed [answers]]]");
+            System.out.println("Row Length: " + args[0] + ", Column Length: " + args[1] + "\nFile: " + args[2] + "\nNo Key");
+            WordSearch p = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), false);
           }
         }
       }
       else{
-        System.out.println("Please input arguments: rows, cols, filename");
+        System.out.println("usage: java WordSearch [rows cols filename [randomSeed [answers]]]");
       }
     }
     catch (NumberFormatException e){
@@ -37,5 +42,6 @@ public class driver4{
     catch(IllegalArgumentException e){
       System.out.println("Row or Column cannot be 0");
     }
+    System.out.println("Changing the seed, dimensions of the puzzle, or quantity of words, even by a single number/word, will cause a completely different puzzle to occur. ");
   }
 }
